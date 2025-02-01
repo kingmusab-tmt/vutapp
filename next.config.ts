e15b50c172ebc90c9942ea3396892adebdbd590d
Musab Mubaraq  Mburaimoh <musab.buraimoh@gmail.com>
2025-02-01 12:40:54 +0100

    loader issue

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,28 @@
 import type { NextConfig } from "next";
-import withTM from 'next-transpile-modules';
+import withTM from "next-transpile-modules";
 
-const nextConfig: NextConfig = withTM(['@simplewebauthn/browser'])({
+const nextConfig: NextConfig = withTM(["@simplewebauthn/browser"])({
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: ["css-loader", "postcss-loader"],
+    });
+
+    return config;
+  },
 });
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+// import withTM from 'next-transpile-modules';
+
+// const nextConfig: NextConfig = withTM(['@simplewebauthn/browser'])({
+//   reactStrictMode: true,
+// });
+
+// export default nextConfig;
 // const nextConfig: NextConfig = {
 //    eslint: {
 //     // Warning: This allows production builds to successfully complete even if
