7aafabe53f3e9e82c4d8474fb0b2f88e585d05ce
Musab Mubaraq  Mburaimoh <musab.buraimoh@gmail.com>
2025-02-01 15:01:25 +0100

    managed api

--- app/components/manageAPI.tsx
+++ app/components/manageAPI.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import UsersPage from "./users";
-import NotificationForm from "./notification";
+// import NotificationForm from "./notification";
 import SendBulkSMS from "./sendSms";
 
 const ApiManagement: React.FC = () => {
@@ -9,7 +9,6 @@ const ApiManagement: React.FC = () => {
 
   const renderSection = () => {
     switch (currentSection) {
-      
       case "Manage API":
         return <UsersPage />;
       case "Add New API":
@@ -25,34 +24,40 @@ const ApiManagement: React.FC = () => {
     <div className="tab">
       <div className="tab-navigation">
         <button
-          className={`tablinks ${currentSection === "Users Details" ? "active" : ""}`}
+          className={`tablinks ${
+            currentSection === "Users Details" ? "active" : ""
+          }`}
           onClick={() => setCurrentSection("Users Details")}
         >
           Users Details
         </button>
         <button
-          className={`tablinks ${currentSection === "Manage User Role" ? "active" : ""}`}
+          className={`tablinks ${
+            currentSection === "Manage User Role" ? "active" : ""
+          }`}
           onClick={() => setCurrentSection("Manage User Role")}
         >
           Manage User Role
         </button>
         <button
-          className={`tablinks ${currentSection === "Notify User(s)" ? "active" : ""}`}
+          className={`tablinks ${
+            currentSection === "Notify User(s)" ? "active" : ""
+          }`}
           onClick={() => setCurrentSection("Notify User(s)")}
         >
           Notify User(s)
         </button>
         <button
-          className={`tablinks ${currentSection === "Send SMS to User(s)" ? "active" : ""}`}
+          className={`tablinks ${
+            currentSection === "Send SMS to User(s)" ? "active" : ""
+          }`}
           onClick={() => setCurrentSection("Send SMS to User(s)")}
         >
           Send SMS to User(s)
         </button>
       </div>
 
-      <div className="tabcontent">
-        {renderSection()}
-      </div>
+      <div className="tabcontent">{renderSection()}</div>
 
       <style jsx>{`
         .tab {
